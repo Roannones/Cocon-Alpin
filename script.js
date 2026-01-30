eaa46495603034751faa188a24cb86ef68bc1290
Roannones <186321086+Roannones@users.noreply.github.com>
2026-01-30 22:05:20 +0100

    Finish the presentation part and creare the slide menu

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const track = document.querySelector(".track");
 const images = document.querySelectorAll(".track img");
+const imageWidth = images[0].getBoundingClientRect().width;
 const nextBtn = document.querySelector("#next");
 
+const menuBtn = document.getElementById('menuBtn');
+const closeBtn = document.getElementById('closeBtn');
+const sideMenu = document.getElementById('sideMenu');
+const overlay = document.getElementById('overlay');
+
 let index = 0;
-const imageWidth = 200;
+/*const imageWidth = 450;*/
 const visibleImages = 3;
 const maxIndex = images.length - visibleImages;
 
 nextBtn.addEventListener("click", () => {
-    console.log("1+");
+
     if (index < maxIndex) {
         index++;
     } else {
@@ -17,3 +23,17 @@ nextBtn.addEventListener("click", () => {
 
     track.style.transform = `translateX(-${index * imageWidth}px)`;
 });
+
+
+menuBtn.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+  overlay.classList.add('open');
+});
+
+closeBtn.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+
+function closeMenu() {
+  sideMenu.classList.remove('open');
+  overlay.classList.remove('open');
+}
